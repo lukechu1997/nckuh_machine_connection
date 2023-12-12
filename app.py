5420e130d29cc6e1bddd23fc9e5ecbcab76d7b82
Luke@home <lukechu3501@gmail.com>
2023-12-12 22:05:09 +0800

    add option dialog

--- app.py
+++ app.py
@@ -1,21 +1,45 @@
 # import pyodbc
 from datetime import date
-from PyQt6.QtWidgets import QMainWindow, QApplication
-import sys, logging
-from libs.view.main import Ui_dialog
-from libs.view.uiFunctions import UiFunc
+from PyQt6.QtWidgets import QMainWindow, QApplication, QDialog
+import logging, os, sys
+from libs.view.ui import Ui_dialog as mainDialog
+from libs.view.main import UiFunc
+from libs.view.option import Ui_Dialog as optionDialog
+
+basicPath = 'C:\\Program Files\\G1200'
 
 FORMAT = '%(asctime)s %(levelname)s: %(message)s'
-logging.basicConfig(level=logging.DEBUG, filename=f'./logs/{date.today()}.log', filemode='a', format=FORMAT)
+logging.basicConfig(level=logging.DEBUG, filename=f'{basicPath}\\logs\\{date.today()}.log', filemode='a', format=FORMAT)
+
+def initializeFolder() :
+    if not os.path.exists(f'{basicPath}'):
+        os.makedirs(f'{basicPath}')
+        print(f'{basicPath} created')
+    if not os.path.exists(f'{basicPath}\\logs'):
+        os.mkdir(f'{basicPath}\\logs')
+        print(f'{basicPath}\\logs created')
+    if not os.path.exists(f'{basicPath}\\dbs'):
+        os.mkdir(f'{basicPath}\\dbs')
+        print(f'{basicPath}\\dbs created')
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.ui = Ui_dialog()
+        self.ui = mainDialog()
         self.ui.setupUi(self)
         UiFunc(self.ui)
+        self.ui.option.clicked.connect(lambda:self.showOption())
+
+    def showOption(self):
+        self.window = QDialog()
+        self.ui = optionDialog()
+        self.ui.setupUi(self.window)
+        self.ui.buttonBox.accepted.connect(lambda:print('accept'))
+        self.window.show()
 
 if __name__ == '__main__':
+    initializeFolder()
     app = QApplication([])
     window = MainWindow()
     window.show()
