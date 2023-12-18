52b399ace947204a15d0060fb6022995d16b9687
Luke@home <lukechu3501@gmail.com>
2023-12-18 08:48:02 +0800

    sync

--- libs/model/sqliteModel.py
+++ libs/model/sqliteModel.py
@@ -3,35 +3,60 @@ import sqlite3
 class SqliteModel:
     def __init__(self):
         try:
-            conn = sqlite3.connect('/Users/lukechu/projects/nckuh_machine_connection/db/g1200.db')
+            conn = sqlite3.connect('./db/g1200.db')
             self.conn = conn
             self.cursorObj = conn.cursor()
         except sqlite3.Error:
             print('error!!!')
             print(sqlite3.Error)
+        
+        self.dbInit()
 
     def dbInit(self):
         self.cursorObj.execute('''
-            CREATE TABLE tests(
+            CREATE TABLE if not exists tests(
                 id INTEGER PRIMARY KEY,
                 name TEXT,
                 serial_no TEXT,
                 create_time DATETIME DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime'))
             );
-            CREATE TABLE results(
+        ''')
+        # 將data拆分
+        self.cursorObj.execute('''
+            CREATE TABLE if not exists results(
                 id INTEGER PRIMARY KEY,
                 data TEXT,
+                uploaded NUMERIC,
                 fk_test_id INTEGER,
                 create_time DATETIME DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')),
                 FOREIGN KEY (fk_test_id) REFERENCES tests(id)
-            )
+            );
         ''')
         self.conn.commit()
 
-    def testFindMany(self):
-        self.cursorObj.execute('SELECT * FROM test')
+    def testsFindMany(self, startDate, endDate):
+        self.cursorObj.execute(f"SELECT * FROM tests WHERE create_time BETWEEN '{startDate}' AND '{endDate}'")
+        names = [description[0] for description in self.cursorObj.description]
         data = self.cursorObj.fetchall()
-        return data
+        dataList = []
+        for item in data:
+            newItem = zip(names, item)
+            dataList.append(dict((x, y) for x, y in newItem))
+
+        self.conn.commit()
+        return dataList
+    
+    def resultsFindMany(self, startDate, endDate):
+        self.cursorObj.execute(f"SELECT * FROM results WHERE create_time BETWEEN '{startDate}' AND '{endDate}'")
+        names = [description[0] for description in self.cursorObj.description]
+        data = self.cursorObj.fetchall()
+        dataList = []
+        for item in data:
+            newItem = zip(names, item)
+            dataList.append(dict((x, y) for x, y in newItem))
+
+        self.conn.commit()
+        return dataList
     
     def listTables(self):
         self.cursorObj.execute('SELECT name FROM sqlite_schema WHERE type ="table" AND name NOT LIKE "sqlite_%";')
@@ -41,7 +66,4 @@ class SqliteModel:
 if __name__ == '__main__':
     print('sqlite connection')
     sql = SqliteModel()
-    if len(sql.listTables()) == 0:
-        sql.dbInit()
-    
-    print(sql.testFindMany())
+    sql.dbInit()
--- libs/view/main.py
+++ libs/view/main.py
@@ -1,28 +1,75 @@
+from ..helpers import serialHelper
 from ..model import mdbModel, sqliteModel
+from datetime import date, timedelta
 from PyQt6.QtWidgets import  QTableWidgetItem, QDialog
-from PyQt6 import QtSerialPort
-from PyQt6.QtCore import QIODeviceBase
+from PyQt6 import QtSerialPort, QtGui
+from PyQt6.QtCore import QIODeviceBase, QDateTime, Qt
+
 # import libs.option as option
 
 class UiFunc:
   def __init__(self, wigets):
     # self.mdbModel = mdbModel.MdbModel()
-    # self.sqliteModel = sqliteModel.SqliteModel()    
+    self.sqliteModel = sqliteModel.SqliteModel()    
     self.wigets = wigets
     self.funcInit()
+    print('current time: ', QDateTime.currentDateTime())
+
+  def __searchResults(self):
+    startDate = self.wigets.testsStartDate.date().toString('yyyy-MM-dd')
+    endDate = self.wigets.testsEndDate.date().toString('yyyy-MM-dd')
+    resultData = self.sqliteModel.resultsFindMany(startDate, endDate)
+    print('result data:', resultData)
+    self.wigets.resultsTable.setRowCount(len(resultData))
+    for index, data in enumerate(resultData):
+      self.wigets.resultsTable.setItem(index, 0, QTableWidgetItem(str(data['id'])))
+      self.wigets.resultsTable.setItem(index, 1, QTableWidgetItem(data['data']))
+
+  def __searchTests(self):
+    startDate = self.wigets.testsStartDate.date().toString('yyyy-MM-dd')
+    endDate = self.wigets.testsEndDate.date().toString('yyyy-MM-dd')
+    # testData = self.mdbModel.testFindMany(startDate, endDate)
+    testData = self.sqliteModel.testsFindMany(startDate, endDate)
+    print('test data:', testData)
+    self.wigets.testsTable.setRowCount(len(testData))
+    for index, data in enumerate(testData):
+      self.wigets.testsTable.setItem(index, 0, QTableWidgetItem(str(data['id'])))
+      self.wigets.testsTable.setItem(index, 1, QTableWidgetItem(data['create_time']))
+    # for index, data in enumerate(testData):
+    #   self.wigets.testsTable.setItem(index, 0, QTableWidgetItem(str(data['SUID'])))
+    #   self.wigets.testsTable.setItem(index, 1, QTableWidgetItem(data['TX_TIME'].strftime('%Y/%m/%d %H:%M:%S')))
+    #   print({
+    #     'id': data['SUID'],
+    #     'txTime': data['TX_TIME'].strftime('%Y/%m/%d %H:%M:%S')
+    #   })
+
+  def __serialReadyRead(self, wigets):
+    buffer = self.serial.read(1024)
+    buffer = buffer.decode("UTF-8")
+    time = QDateTime.currentDateTime().toString("yyyy-MM-dd hh:mm:ss  ")
+    wigets.textBrowser.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+    tc = self.textBrowser.textCursor()
+    tc.movePosition(QtGui.QTextCursor.MoveOperation.End)
+    tc.insertText(time + buffer)
+
+  def __queryDateTimeInit(self):
+    startDate = date.today()
+    endDate = date.today() + timedelta(days=1)
+    self.wigets.testsStartDate.setDate(startDate)
+    self.wigets.testsEndDate.setDate(endDate)
     
-  def serialInit(self, uiWigets):
+  def serialInit(self, wigets):
     availablePorts = QtSerialPort.QSerialPortInfo.availablePorts()
     for port in availablePorts:
-      uiWigets.serialPortOption.addItem(port.portName())
+      wigets.serialPortOption.addItem(port.portName())
     
     self.serial = QtSerialPort.QSerialPort()
 
-    # serialPortName = uiWigets.serialPortOption.currentText()
-    uiWigets.buttonBox.accepted.connect(lambda:self.changeSerialPortName(uiWigets.serialPortOption.currentText()))
-    
+    wigets.selectPort.clicked.connect(lambda:self.changeSerialPortName(wigets.serialPortOption.currentText()))
+    self.serial.readyRead.connect(self.__serialReadyRead(wigets))
+
   def changeSerialPortName(self, port): 
-    print(f'change serial port to {port}')
+    # print(f'change serial port to {port}')
     if(self.serial.isOpen()):
       self.serial.close()
 
@@ -37,33 +84,19 @@ class UiFunc:
     if not self.serial.open(QIODeviceBase.OpenModeFlag.ReadWrite):
       print("错误", "打开串口失败:" + self.serial.errorString())
 
+    # serialHelper(self.serial)
+    # self.serial.readyRead.connect(self.__serialReadyRead())
+
   def onClickConfirmBtn(self):
     print('clicked')
 
-  def onClickSearchTests(self):
-    startDate = self.wigets.testsStartDate.date().toString('yyyy/MM/dd')
-    endDate = self.wigets.testsEndDate.date().toString('yyyy/MM/dd')
-    testData = self.mdbModel.testFindMany(startDate, endDate)
-    self.wigets.testsTable.setRowCount(len(testData))
-    for index, data in enumerate(testData):
-      self.wigets.testsTable.setItem(index, 0, QTableWidgetItem(str(data['SUID'])))
-      self.wigets.testsTable.setItem(index, 1, QTableWidgetItem(data['TX_TIME'].strftime('%Y/%m/%d %H:%M:%S')))
-      print({
-        'id': data['SUID'],
-        'txTime': data['TX_TIME'].strftime('%Y/%m/%d %H:%M:%S')
-      })
-
-  # def onClickOptionBtn(self):
-  #   # print('open option dialog')
-  #   # self.wigets.optionUi.setupUi(self)
-  #   # self.exec_()
-  #   dialog = QDialog()
-  #   dialog.ui = option.Ui_Dialog()
-  #   dialog.ui.setupUi(dialog)
-  #   dialog.exec_()
+  def onClickSearchBtn(self):
+    self.__searchTests()
+    self.__searchResults()
 
   def funcInit(self): 
     print('func init')
+    self.__queryDateTimeInit()
     # self.wigets.option.clicked.connect(lambda:self.onClickOptionBtn())
     # self.wigets.confirmBtn.clicked.connect(lambda:self.onClickConfirmBtn())
-    # self.wigets.searchTests.clicked.connect(lambda:self.onClickSearchTests())
+    self.wigets.searchBtn.clicked.connect(lambda:self.onClickSearchBtn())
