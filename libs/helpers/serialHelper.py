bc2a45c2c5c86949ee3122b86d6364364f48c162
Luke Chu <luke.chu@eucare.com.tw>
2023-12-20 18:37:45 +0800

    update serial helper

--- libs/helpers/serialHelper.py
+++ libs/helpers/serialHelper.py
@@ -1,4 +1,4 @@
-import datetime
+import logging
 
 class SerialHelper:
   def __init__(self, serial):
@@ -9,6 +9,40 @@ class SerialHelper:
     match data:
       case "ENQ":
         self.serial.write("ACK".encode("UTF-8"))
+      case _:
+        self.readComplicatedData(data)
+
+  def readComplicatedData(self, data):
+    logging.info(data)
+    print(data)
+    dataList = str.split(' ')
+
+    match dataList[0]:
+      case 'R':
+        print('R')
+      case 'D':
+        print('D')
+      case 'S':
+        print('S')
+      case _:
+        print('other')
+
+  def sendRequestMsg(self, data = {}):
+    messageBodyDict = {
+      "messageId": "W"
+    }
+
+    messageBodyStr = ' '.join(messageBodyDict.values())
+    self.serial.write(messageBodyStr.encode("UTF-8"))
+
+  def sendStatusQuery(self): 
+    self.serial.write('Q 1'.encode("UTF-8"))
+
+  def receiveResult(self, data):
+    keys = [
+      'message_id', 'analyzer_id', 'specimen_category', 'sample_no', 'sequence_no', 
+      'patient_id', 'reck_id', 'position', 'sample_type', 
+    ]
 
 if __name__ == '__main__':
     print('serial helper')
